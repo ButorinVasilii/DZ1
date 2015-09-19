28c7fdcd87d069ca8b254ed8486d9636ee0e6473
ButorinVasilii <mailboxva@yandex.ru>
2015-09-19 11:45:29 +0500

    Чтение из буфера

--- ConsoleApplication65/ConsoleApplication65/Program.cs
+++ ConsoleApplication65/ConsoleApplication65/Program.cs
@@ -3,13 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace ConsoleApplication65
 {
     class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
+            var text = Clipboard.GetText();
+            Console.WriteLine(text);
+            Console.Read();
+
         }
     }
 }
